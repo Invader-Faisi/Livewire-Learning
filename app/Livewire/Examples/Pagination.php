2137795dd59c93f3dbef1b4c5c501de53e4258db
Invader-Faisi <code.pedia.tube@gmail.com>
2024-01-14 12:59:52 +0500

    pagination

--- app/Livewire/Examples/Pagination.php
+++ app/Livewire/Examples/Pagination.php
@@ -9,10 +9,26 @@ use Livewire\WithPagination;
 class Pagination extends Component
 {
     use WithPagination;
+
+    public $query;
+
+    public $pagination = 10;
+
     public function render()
     {
+        if (!$this->query) {
+            $users = User::paginate($this->pagination);
+        } else {
+            $users = User::where('name', 'like', '%' . $this->query . '%')
+                ->orWhere('name', 'like', '%' . $this->query . '%')->simplePaginate($this->pagination);
+        }
         return view('livewire.examples.pagination', [
-            'users' => User::paginate(5)
+            'users' => $users
         ]);
+    }
+
+    public function updatequery()
+    {
+        $this->resetPage();
     }
 }
--- resources/views/livewire/examples/pagination.blade.php
+++ resources/views/livewire/examples/pagination.blade.php
@@ -1,5 +1,18 @@
 <div class="container">
     <h1>All User Paginate</h1>
+    <div class="input-group">
+        <div class="form-outline" data-mdb-input-init>
+            <input type="text" wire:model.live='query' class="form-control" />
+            <label class="form-label" for="form1">Search</label>
+        </div>
+            <select class="select border border-success" wire:model.live='pagination' style="height: 40px">
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+            </select>
+    </div>
+    
     <table class="table">
         <thead>
             <tr>
